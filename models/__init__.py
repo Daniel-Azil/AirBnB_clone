ff70e697cb06eb063a1bca560bcdbea2d496a1a8
Daniel Azil <danielazil.fse@gmail.com>
2023-10-13 16:28:15 +0000

    removed file

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Initialize the storage module for easy access."""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-
-
-storage = FileStorage()
-
