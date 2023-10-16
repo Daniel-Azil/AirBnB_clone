b6b11a8cc142bbab70410a5cd74b38768d9cb863
Daniel Azil <danielazil.fse@gmail.com>
2023-10-16 02:21:41 +0000

    made some corrextions

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,9 +3,9 @@
     A module that is used in testing the functionality
     of the console.
 """
-from unittest.mock import patch 
-import tests
+from unittest.mock import patch
 import console
+import test
 import unittest
 import json
 import os
@@ -28,113 +28,116 @@ class Test_HBNBConsole(unittest.TestCase):
 
     def test_noLines(self):
         """Method test case for empty lines"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("\n")
-            self.assertEqual(strIOformat.getvalue(), "")  
-  
-    @classmethod
-    def initialise_instance(self):
-        """Method test case initialise an instance"""
-        self.commandclass = console.HBNBCommand()
-:
+            commandClass.onecmd("\n")
+            self.assertEqual(strIOformat.getvalue(), "")
+
     def test_documentations(self):
         """Method test case for DocStrings"""
+        commandClass = console.HBNBCommand()
         self.assertTrue(len(self.__doc__) >= 1)
         self.assertTrue(len(console.__doc__) >= 1)
 
     def test_commandCount(self):
         """Method test case for count"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("State.count()")
+            commandClass.onecmd("State.count()")
             self.assertEqual(int, type(eval(strIOformat.getvalue())))
 
     def test_commandCreate(self):
         """Method test case for create command"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("create")
+            commandClass.onecmd("create")
             self.assertEqual("** class name missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("create Qqqqq")
+            commandClass.onecmd("create Qqqqq")
             self.assertEqual("** class doesn't exist **\n",
                              strIOformat.getvalue())
 
     def test_cmdddestroy(self):
         """Method test case for destroy command"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("destroy")
+            commandClass.onecmd("destroy")
             self.assertEqual("** class name missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("destroy QQQQWWWW")
+            commandClass.onecmd("destroy QQQQWWWW")
             self.assertEqual("** class doesn't exist **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("destroy Review")
+            commandClass.onecmd("destroy Review")
             self.assertEqual("** instance id missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("destroy User r934r83hfff")
+            commandClass.onecmd("destroy User r934r83hfff")
             self.assertEqual("** no instance found **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("BaseModel.destroy('f5g77g')")
+            commandClass.onecmd("BaseModel.destroy('f5g77g')")
             self.assertEqual("** no instance found **\n",
-                             strIOformat.getvalue())  
+                             strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("create Review")
-            self.commandclass.onecmd("create Review") 
+            commandClass.onecmd("create Review")
+            commandClass.onecmd("create Review")
 
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("Review.all()")
+            commandClass.onecmd("Review.all()")
             self.assertEqual("[[Review]",
                              strIOformat.getvalue()[:9])
 
     def test_commandAll(self):
         """Method test case for all command"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("all QqqqFfff")
+            commandClass.onecmd("all QqqqFfff")
             self.assertEqual("** class doesn't exist **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("all BaseModel")
+            commandClass.onecmd("all BaseModel")
             self.assertEqual("[]\n", strIOformat.getvalue())
 
     def test_commandShow(self):
         """Method test case for show command"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("show")
+            commandClass.onecmd("show")
             self.assertEqual("** class name missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("test.show()")
+            commandClass.onecmd("test.show()")
             self.assertEqual("** class doesn't exist **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("show BaseModel")
+            commandClass.onecmd("show BaseModel")
             self.assertEqual("** instance id missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("Place.show('f5g77g')")
+            commandClass.onecmd("Place.show('f5g77g')")
             self.assertEqual("** no instance found **\n",
                              strIOformat.getvalue())
 
     def test_cmdupdate(self):
         """Method test case upate command"""
+        commandClass = console.HBNBCommand()
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("update")
+            commandClass.onecmd("update")
             self.assertEqual("** class name missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("update QQQFFFF")
+            commandClass.onecmd("update QQQFFFF")
             self.assertEqual("** class doesn't exist **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("update Review")
+            commandClass.onecmd("update Review")
             self.assertEqual("** instance id missing **\n",
                              strIOformat.getvalue())
         with patch('sys.stdout', new=StringIO()) as strIOformat:
-            self.commandclass.onecmd("update Review r934r83hfff")
+            commandClass.onecmd("update Review r934r83hfff")
             self.assertEqual("** no instance found **\n",
                              strIOformat.getvalue())
 
